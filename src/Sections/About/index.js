ed9875b5553a083199eb4c8ccb6265f70ceede43
Ankesh <ank.paramanik@gmail.com>
2021-06-30 21:40:24 +0530

    About section content added Added content to About section Created dummy works data for Work section

--- src/Sections/About/index.js
+++ src/Sections/About/index.js
@@ -1,14 +1,51 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import Skills from "./skills.json";
+import './about-styles.scss';
+import Photo from './../../assets/portfolio_image.png';
 
 const About = () => {
     return (
-        <Container
-            id="about"
-            fluid
-            className="wrapper section--wrapper__about"
-        >
+        <Container id="about" fluid className="wrapper section--wrapper__about">
             <h2 className="section--header">About</h2>
+            <Row>
+                <Col sm="8">
+                    <Row>
+                        <Col sm={12}>
+                            <p>
+                                My name is Ankesh I enjoy developing web
+                                applications and learning new web technologies.
+                                I developed interest in web development back in
+                                2015 when I attended a workshop in my college.
+                            </p>
+                            <p>
+                                At present, I am working as a frontend developer
+                                in an MNC where I focus on building
+                                user-friendly yet informative dashboards for a
+                                variety of clients.
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row className="skills--row">
+                        <Col sm={12}>
+                            <p>
+                                <strong>
+                                    Here are a few technologies I've been
+                                    working with:
+                                </strong>
+                            </p>
+                            <ul className="skills--list p0">
+                                {Skills.map((skill) => (
+                                    <li key={skill} className="list__item">{skill}</li>
+                                ))}
+                            </ul>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col sm="4" className="about--col__image">
+                    <img src={Photo} alt="Ankesh Paramanik" className="about-content--image" />
+                </Col>
+            </Row>
         </Container>
     );
 };
---
+++ src/Sections/Work/works.js
@@ -0,0 +1,40 @@
+const works = {
+    top: [
+        {
+            title: "Tirent Designs",
+            tech: ["HTML5", "CSS", "AngularJS"],
+            description: ["desc para 1", "desc para 2"],
+            slides: [
+                "img1", "img2", "video"
+            ]
+        },
+        {
+            title: "Tirent Designs2",
+            tech: ["HTML5", "CSS", "AngularJS"],
+            description: ["desc para 1", "desc para 2"],
+            slides: [
+                "img1", "img2", "video"
+            ]
+        },
+        {
+            title: "Tirent Designs3",
+            tech: ["HTML5", "CSS", "AngularJS"],
+            description: ["desc para 1", "desc para 2"],
+            slides: [
+                "img1", "img2", "video"
+            ]
+        },
+        {
+            title: "Tirent Designs4",
+            tech: ["HTML5", "CSS", "AngularJS"],
+            description: ["desc para 1", "desc para 2"],
+            slides: [
+                "img1", "img2", "video"
+            ]
+        }
+    ],
+    more: [], // more dashboards
+    others: [] // code pens
+};
+
+export default works;
