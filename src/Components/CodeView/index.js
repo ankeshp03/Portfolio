1b9deab1a3dfdddbeb72a3d1cd6afd0d28f7c2ee
Ankesh <ank.paramanik@gmail.com>
2021-07-10 21:55:04 +0530

    CodeView unused lines commented

--- src/Components/CodeView/index.js
+++ src/Components/CodeView/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-const generateLines = (lineCount) => {
-    let lines = [];
-    for (let line = 0; line < lineCount; line++) {
-        const lineSegmentCount = Math.floor(Math.random()) * 3 + 3;
-        const lineSegments = [];
-        let totalWidth = 0;
+// const generateLines = (lineCount) => {
+//     let lines = [];
+//     for (let line = 0; line < lineCount; line++) {
+//         const lineSegmentCount = Math.floor(Math.random()) * 3 + 3;
+//         const lineSegments = [];
+//         let totalWidth = 0;
 
-        for (let segment = 0; segment < lineSegmentCount; segment++) {
-            lineSegments.push(<span></span>);
-        }
-        lines.push();
-    }
-};
+//         for (let segment = 0; segment < lineSegmentCount; segment++) {
+//             lineSegments.push(<span></span>);
+//         }
+//         lines.push();
+//     }
+// };
 
 const CodeView = () => {
-    const lineCount = 25;
+    // const lineCount = 25;
     return (
         <div className="container__code-view">
             <div></div>
