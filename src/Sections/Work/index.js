ba1e9e53f945df667f345e6c10f681bd640de9f7
Ankesh <ank.paramanik@gmail.com>
2021-07-17 23:19:59 +0530

    Work cards style changes

--- src/Sections/Work/index.js
+++ src/Sections/Work/index.js
@@ -12,49 +12,69 @@ const Work = () => {
             <div className="work--card__container">
                 <Card className="work--card">
                     <Card.Body>
-                        <Card.Img variant="top" src="" />
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Card.Img
+                            src="http://lorempixel.com/400/200"
+                        />
+                        <div className="work--card__text-content mT10">
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </div>
                     </Card.Body>
                 </Card>
 
                 <Card className="work--card">
                     <Card.Body>
-                        <Card.Img variant="top" src="" />
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Card.Img
+                            src="http://lorempixel.com/400/200"
+                        />
+                        <div className="work--card__text-content mT10">
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </div>
                     </Card.Body>
                 </Card>
 
                 <Card className="work--card">
                     <Card.Body>
-                        <Card.Img variant="top" src="" />
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Card.Img
+                            src="http://lorempixel.com/400/200"
+                        />
+                        <div className="work--card__text-content mT10">
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </div>
                     </Card.Body>
                 </Card>
 
                 <Card className="work--card">
                     <Card.Body>
-                        <Card.Img variant="top" src="" />
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Card.Img
+                            src="http://lorempixel.com/400/200"
+                        />
+                        <div className="work--card__text-content mT10">
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </div>
                     </Card.Body>
                 </Card>
             </div>
